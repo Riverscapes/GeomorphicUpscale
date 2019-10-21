f212f4e97dde20d4baf9a977e01f553c279cc9ce
WHTNLAB-6MCHHK2\SaraBangen <sara.bangen@gmail.com>
2019-10-20 23:27:31 -0700

    Adding separate networks for chinook and steelehad

--- SalmonExample/UpscaleWrapper.R
+++ SalmonExample/UpscaleWrapper.R
@@ -8,21 +8,21 @@
 # Set required paths ---------------------------
 
 # User defined project directory path where outputs will be written
-proj.dir = "C:/Anabranch/UpperSalmon/wrk_Data/GUTUpscale/PrelimRun_01"
+proj.dir = "C:/Anabranch/UpperSalmon/wrk_Data/GUTUpscale/PrelimRun_05"
 
 # Specify directory path to the downloaded Git Repo
 repo.dir = "C:/etal/LocalCode/GeomorphicUpscale"
 
 # Path to selections csv created by RSselections.R
-selections.file = "C:/Anabranch/UpperSalmon/wrk_Data/GUTUpscale/PrelimRun_01/Inputs/Selections.csv"
+selections.file = "C:/Anabranch/UpperSalmon/wrk_Data/GUTUpscale/PrelimRun_05/Inputs/Selections.csv"
 
 # Path to network csv and braid index csv
-network.file = "C:/Anabranch/UpperSalmon/wrk_Data/GUTUpscale/PrelimRun_01/Inputs/Anadramous_Network.csv"
+chinook.network.file = "C:/etal/LocalCode/GeomorphicUpscale/SalmonExample/Chinook_Network.csv"
+steelhead.network.file = "C:/etal/LocalCode/GeomorphicUpscale/SalmonExample/Steelhead_Network.csv"
 braid.index.file = NA
 
 # User defined variables ---------------------------
 
-gu.type = "UnitForm"      #Options: UnitForm, GU # UnitShape not available at this time since I don't have maps of these in the database
 RSlevels = NA # optional vector argument to set RS factor order in graphs and displays, leave as  NA if alphabetical is desired
 geoindicators = c('BfBraid', 'LWFreq_Wet', 'SlowWater_Freq', 'ChnlUnit_Freq', 'Sin') # list of geoindicators to create plots for
 
@@ -36,7 +36,7 @@ selections = read_csv(selections.file)
 
 # Builds the project directory structure and re-organizes inputs ---------------------------
 
-source(file.path(repo.dir, "scripts/projbuild.R"))
+# source(file.path(repo.dir, "scripts/projbuild.R"))
 
 
 # Generate selection output ---------------------------
@@ -51,7 +51,6 @@ source(file.path(repo.dir, "scripts/selection.geoindicators.R"))
 
 
 # Generate assemblage output ---------------------------
-# NK Note: plots are not printing????
 
 # output per reach
 source(file.path(repo.dir, "scripts/assemblage.reach.R"))
@@ -71,20 +70,47 @@ source(file.path(repo.dir, "scripts/response.unit.R"))
 
 # Generate upscale output ---------------------------
 
-response.pool = "byRSCond"     #Options: "byRScond", "byRS", "byAll"
+gu.type = "GU" 
+response.pool = "byRSCond"     # Options: "byRScond", "byRS", "byAll"
 seg.id.col = "RECNO"        
 length.col = "LENGTH"        
 width.col = "WIDE_WW"       
-cond.cols = c("GEO_COND")     #user supplied
-area.col = "AREA_WW" # leave as NA if no area is specified per reach segment and it will be estimated but this requires braid index file
-
-# read in network and braid.index files
-network = read_csv(network.file)
-
-# filter out rows with missing widths
-network = network %>% filter(WIDE_WW > 0)
+cond.cols = c("COND_HIST", "COND_EX", "COND_RP")     #user supplied
+area.col = "AREA_BF" # leave as NA if no area is specified per reach segment and it will be estimated but this requires braid index file
 
 if(!is.na(braid.index.file)){braid.index = read_csv(braid.index.file)}else{braid.index = NA}
 
-# source and run code to generate output for upscaling response variabales
+# read in chinook nework and filter out rows with missing widths
+chinook.network = read_csv(chinook.network.file) %>% filter(WIDE_WW > 0)
+
+# run for chinook spawners
+species = "chinook"   
+model = "fuzzy"   
+lifestage = "spawner"
+network = chinook.network %>% filter(SPAWN == 1)
 source(file.path(repo.dir, "scripts/upscale.response.R"))
+
+# run for chinook juveniles
+species = "chinook" 
+model = "fuzzy"    
+lifestage = "juvenile"
+network = chinook.network %>% filter(REAR == 1)
+source(file.path(repo.dir, "scripts/upscale.response.R"))
+
+# read in steelhead nework and filter out rows with missing widths
+steelhead.network = read_csv(steelhead.network.file) %>% filter(WIDE_WW > 0)
+
+# run for steelhead spawners
+species = "steelhead"   
+model = "fuzzy"   
+lifestage = "spawner"
+network = steelhead.network %>% filter(SPAWN == 1)
+source(file.path(repo.dir, "scripts/upscale.response.R"))
+
+# run for steelhead juveniles
+species = "steelhead" 
+model = "nrei"    
+lifestage = "juvenile"
+network = steelhead.network %>% filter(REAR == 1)
+source(file.path(repo.dir, "scripts/upscale.response.R"))
+
